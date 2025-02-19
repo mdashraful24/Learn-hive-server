4c52e7fe616775dc1139fceb3f82387c69b6d954
mdashraful24 <mdashrafulislam2882@gmail.com>
2025-02-19 19:29:11 +0600

    added all classes api modify

--- index.js
+++ index.js
@@ -409,6 +409,7 @@ async function run() {
             }
         });
 
+        
         app.get('/all-classes/:id', async (req, res) => {
             const id = req.params.id;
             const query = { _id: new ObjectId(id) }
